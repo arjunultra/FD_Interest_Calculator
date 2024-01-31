6a349d7dce31ff67143ed7b0fe15b03607939979
Arjun Arasu <arjunbalan1994@gmail.com>
2024-01-31 12:23:07 +0530

    project done

--- fd_calc.js
+++ fd_calc.js
@@ -3,12 +3,16 @@ const calculateBtn = document.getElementById("calculate-btn");
 calculateBtn.addEventListener("click", function () {
   clearResult();
   let inputRate = document.getElementById("input-rate");
-  if (inputRate.value <= 3) {
-    resultAns.innerHTML += ` 5.8%`;
-  } else if (inputRate.value >= 6) {
-    resultAns.innerHTML += ` 6.5%`;
+  if (inputRate.value < 3) {
+    resultAns.innerHTML = `5.8%`;
+  } else if (inputRate.value >= 3 && inputRate.value <= 6) {
+    resultAns.innerHTML = `6.5%`;
+  } else if (inputRate.value >= 7 && inputRate.value <= 9) {
+    resultAns.innerHTML = `6.8%`;
+  } else {
+    resultAns.innerHTML = `7%`;
   }
 });
 function clearResult() {
-  resultAns.innerText += "";
+  resultAns.innerHTML = "";
 }
